b59dba10dfa3a6126df540ba0f2608a6f2b2f4e0
NATkachuk <nazartkacuk301@gmail.com>
2025-01-29 13:51:32 +0300

    fix rout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import Layout from './pages/layout/Layout'
 import About from './pages/About/About'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 
 function App() {
 
   return (
-    <HashRouter basename="/Drivoxe">
+    <BrowserRouter basename="/Drivoxe">
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="About" element={<About />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 export default App
